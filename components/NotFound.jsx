351319964f5b2d73f2df4d18c9919bb41ecd2766
Erik <erik@processmaker.com>
2015-08-15 16:10:56 -0500

    adding NotFound component

---
+++ components/NotFound.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+export default class NotFound extends React.Component {
+    render () {
+        return (
+            <p>404 - NOT FOUND.</p>
+        );
+    }
+}
+
+
